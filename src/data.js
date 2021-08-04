ea1a0bfe4cd5fdac691691857f8e8dfc23a7073a
Owner <owner@Owners-MacBook-Pro.local>
2021-08-03 23:36:47 -0700

    Basic app setup

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const DATA = [
+    {
+        title: 'Grain to Glass',
+        name: 'Eimverk Distillery',
+        address: {
+            city: 'Reykjavik',
+            country: 'Iceland'
+        }
+    },
+    {
+        title: 'Bean to Bar'
+    },
+    {
+        title: 'Farm to Barn',
+        name: 'Farm to Barn Cafe',
+        address: {
+            city: 'Oahu',
+            state: 'HI',
+            country: 'USA'
+        }
+    },
+    {
+        title: 'Farm to Taco',
+        name: 'Tacodeli',
+        date: new Date(2021, 6, 24),
+        address: {
+            city: 'Austin',
+            state: 'TX',
+            country: 'USA'
+        }
+    },
+    {
+        title: 'Farm to Barrel',
+        date: new Date(2021, 6, 28),
+        address: {
+            city: 'Mountain View',
+            state: 'CA',
+            country: 'USA'
+        }
+    }
+];
--- src/index.js
+++ src/index.js
@@ -1,59 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { DATA } from './data.js';
+import './index.scss';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
+const pad = (num, digits) => {
+  num = num.toString();
+  while (num.length < digits) num = "0" + num;
+  return num;
+}
+
+class FieldLabel extends React.Component {
+  render() {
+    return (
+      <span class="fieldLabel">{this.props.text}</span>
+    )
   }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
+}
+
+class TicketDate extends React.Component {
+  render() {
+    return (
+      <div className="date">
+        <FieldLabel text="Date of violation"/>
+        <span className="year">{this.props.date ? this.props.date.getFullYear() : ' '}</span>
+        / <span className="month">{this.props.date ? pad(this.props.date.getMonth() + 1, 2) : ' '}</span>
+        / <span className="day">{this.props.date? pad(this.props.date.getDate(), 2) : ' '}</span>
+      </div>
+    )
   }
+}
+
+class Ticket extends React.Component {
   
-  class Game extends React.Component {
+    getCitationNumber(num) {
+      return pad(num, 4);
+    }
+
     render() {
+        return (
+            <div className="ticket">
+                <div className="field">
+                  <div className="title">BULLSH*T CITATION</div>
+                </div>
+                <div className="field">
+                  <div className="citationNumber">
+                    <FieldLabel text="Citation No." />
+                    #{this.getCitationNumber(this.props.id)}
+                  </div>
+                </div>
+                <div className="field">
+                <FieldLabel text="Offense" />
+                  <div className="offense">
+                    "{this.props.data.title}"
+                  </div>
+                </div>
+                <div className="field">
+                  <FieldLabel text="Name" />
+                  <div className="name">
+                    {this.props.data.name}
+                  </div>
+                </div>
+                <div className="field">
+                  <TicketDate date={this.props.data.date} />
+                </div>
+            </div>
+        )
+    }
+}
+  
+  class BsApp extends React.Component {
+    render() {
+        const tickets = DATA.map((item, i) => 
+            <Ticket data={item} id={i} key={`ticket-${i}`} />
+        );
+        console.log(tickets);
       return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+        <div className="app">
+          {tickets}
         </div>
       );
     }
@@ -62,7 +85,50 @@ class Square extends React.Component {
   // ========================================
   
   ReactDOM.render(
-    <Game />,
+    <BsApp />,
     document.getElementById('root')
   );
+
+// ========================================
+
+// class Square extends React.Component {
+//     render() {
+//       return (
+//         <button className="square">
+//           {/* TODO */}
+//         </button>
+//       );
+//     }
+//   }
+  
+//   class Board extends React.Component {
+//     renderSquare(i) {
+//       return <Square />;
+//     }
+  
+//     render() {
+//       const status = 'Next player: X';
+  
+//       return (
+//         <div>
+//           <div className="status">{status}</div>
+//           <div className="board-row">
+//             {this.renderSquare(0)}
+//             {this.renderSquare(1)}
+//             {this.renderSquare(2)}
+//           </div>
+//           <div className="board-row">
+//             {this.renderSquare(3)}
+//             {this.renderSquare(4)}
+//             {this.renderSquare(5)}
+//           </div>
+//           <div className="board-row">
+//             {this.renderSquare(6)}
+//             {this.renderSquare(7)}
+//             {this.renderSquare(8)}
+//           </div>
+//         </div>
+//       );
+//     }
+//   }
   
